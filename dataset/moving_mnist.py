4d34ee123d779b3e3c552927db22301d15650a97
tsugumi-sys <tidemark0105@gmail.com>
2022-11-28 14:42:48 +0900

    add data loader

--- dataset/moving_mnist.py
+++ dataset/moving_mnist.py
@@ -1,98 +1,143 @@
+import codecs
 import os
-from typing import Any, Callable, List, Optional, Tuple
+import os.path
+import shutil
+import string
+import sys
+import warnings
+from typing import Any, Callable, Dict, List, Optional, Tuple
+from urllib.error import URLError
 
+import numpy as np
 import torch
-import torch.utils.data as data
+from torchvision.datasets.utils import (
+    download_url,
+)
+from torchvision.datasets.vision import VisionDataset
 
-from .utils import _log_api_usage_once
 
+class MovingMNIST(VisionDataset):
+    """`MovingMNIST <http://www.cs.toronto.edu/~nitish/unsupervised_video/>`_ Dataset.
+    Args:
+        root (string): Root directory of dataset where ``raw/mnist_test_seq.npy`` exists.
+        train (bool, optional): If True, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in an PIL
+            image and returns a transformed version. E.g, ``transforms.RandomCrop``
+    """
 
-class VisionDataset(data.Dataset):
-    _repr_indent = 4
+    url = "http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy"
+    md5 = "be083ec986bfe91a449d63653c411eb2"
 
     def __init__(
         self,
         root: str,
-        transforms: Optional[Callable] = None,
+        input_seq_length: int,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
+        download: bool = False,
     ) -> None:
-        _log_api_usage_once(self)
-        if isinstance(root, torch._six.string_classes):
-            root = os.path.expanduser(root)
-        self.root = root
+        super().__init__(root, transform=transform, target_transform=target_transform)
 
-        has_transforms = transforms is not None
-        has_separate_transform = transform is not None or target_transform is not None
-        if has_transforms and has_separate_transform:
+        if input_seq_length > 19 or 1 > input_seq_length:
             raise ValueError(
-                "Only transforms or transform/target_transform can be passed as argument."
+                "input_seq_length should be in the range of (1, 19) because the original sequence length is 20."
+            )
+        self.input_seq_length = input_seq_length
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError(
+                "Dataset not found. You can use download=True to download it."
             )
 
-        # for backwards-compatibility
-        self.transform = transform
-        self.target_transform = target_transform
+        self.data, self.targets = self._load_data()
 
-        if has_separate_transform:
-            transforms = StandardTransform(transform, target_transform)
-        self.transforms = transforms
+    def _load_data(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: The input tensors and label tensors splited by time. Each tensor has the shape like (channel, sequence_length, height, width).
+        """
+        data = torch.from_numpy(
+            np.load(os.path.join(self.raw_folder, self.raw_filename))
+        )  # data has the shape of (sequence length, batch_size, height, width)
+        data = torch.swapaxes(data, 0, 1)
+        batch_size, seq_len, height, width = data.size()
+        data = torch.reshape(data, (batch_size, 1, seq_len, height, width))
+        return data[:, :, : self.input_seq_length], data[:, :, self.input_seq_length :]
 
-    def __getitem__(self, index: int) -> Any:
-        raise NotImplementedError
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (data, targets) where sampled sequences are splitted into a data
+                    and targets part
+        """
+        data, targets = self.data[idx], self.targets[idx]
+        if self.transform is not None:
+            data = self._transform_sequences(data, self.transform)
+
+        if self.target_transform is not None:
+            targets = self._transform_sequences(targets, self.target_transform)
+
+        return data, targets
+
+    def _transform_sequences(
+        self, img_sequences: torch.Tensor, transform: Callable
+    ) -> torch.Tensor:
+        """
+        Args:
+            img_sequences (torch.Tensor): Tensor of image sequences (channel, sequence_length, height, width)
+            transform (Calllable): transform function.
+        Returns:
+            torch.Tensor: Transformed tensors (channel, sequence_length, height, width)
+        """
+        for seq_idx in range(img_sequences.size(1)):
+            img_sequences[:, seq_idx] = transform(img_sequences[:, seq_idx])
+        return img_sequences
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return len(self.data)
 
-    def __repr__(self) -> str:
-        head = "Dataset " + self.__class__.__name__
-        body = [f"Number of datapoints: {self.__len__()}"]
-        if self.root is not None:
-            body.append(f"Root location: {self.root}")
-        body += self.extra_repr().splitlines()
-        if hasattr(self, "transforms") and self.transforms is not None:
-            body += [repr(self.transforms)]
-        lines = [head] + [" " * self._repr_indent + line for line in body]
-        return "\n".join(lines)
+    def _check_exists(self) -> bool:
+        return os.path.exists(os.path.join(self.raw_folder, self.raw_filename))
 
-    def _format_transform_repr(self, transform: Callable, head: str) -> List[str]:
-        lines = transform.__repr__().splitlines()
-        return [f"{head}{lines[0]}"] + [
-            "{}{}".format(" " * len(head), line) for line in lines[1:]
-        ]
+    def download(self) -> None:
+        """Download the MovingMNIST data if it doesn't exist already."""
 
-    def extra_repr(self) -> str:
-        return ""
+        if self._check_exists():
+            return
 
-
-class StandardTransform:
-    def __init__(
-        self,
-        transform: Optional[Callable] = None,
-        target_transform: Optional[Callable] = None,
-    ) -> None:
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __call__(self, input: Any, target: Any) -> Tuple[Any, Any]:
-        if self.transform is not None:
-            input = self.transform(input)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return input, target
-
-    def _format_transform_repr(self, transform: Callable, head: str) -> List[str]:
-        lines = transform.__repr__().splitlines()
-        return [f"{head}{lines[0]}"] + [
-            "{}{}".format(" " * len(head), line) for line in lines[1:]
-        ]
-
-    def __repr__(self) -> str:
-        body = [self.__class__.__name__]
-        if self.transform is not None:
-            body += self._format_transform_repr(self.transform, "Transform: ")
-        if self.target_transform is not None:
-            body += self._format_transform_repr(
-                self.target_transform, "Target transform: "
+        # Download file
+        try:
+            print(f"Downloading {self.url}")
+            download_url(
+                self.url,
+                root=self.raw_folder,
+                filename=self.raw_filename,
+                md5=self.md5,
             )
+        except URLError as error:
+            print(f"Failed to download:\n{error}")
+        finally:
+            print()
 
-        return "\n".join(body)
+    @property
+    def raw_folder(self) -> str:
+        return os.path.join(self.root, self.__class__.__name__, "raw")
+
+    @property
+    def raw_filename(self) -> str:
+        return self.url.split("/")[-1]
+
+
+if __name__ == "__main__":
+    dataset = MovingMNIST(root=".", input_seq_length=10, download=True)
