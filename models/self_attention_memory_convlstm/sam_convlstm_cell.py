4f65e7f3cdd8a8f53232941acc3d23edd5a2db0a
tsugumi-sys <tidemark0105@gmail.com>
2022-12-27 12:37:22 +0900

    delete x

--- models/self_attention_memory_convlstm/sam_convlstm_cell.py
+++ models/self_attention_memory_convlstm/sam_convlstm_cell.py
@@ -45,7 +45,6 @@ class SAMConvLSTMCell(BaseConvLSTMCell):
         prev_cell: torch.Tensor,
         prev_memory: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        X = self.attention_x(X)
         new_h, new_cell = self.convlstm_cell(X, prev_h, prev_cell)
         new_h, new_memory = self.attention_memory(new_h, prev_memory)
         return new_h.to(DEVICE), new_cell.to(DEVICE), new_memory.to(DEVICE)
