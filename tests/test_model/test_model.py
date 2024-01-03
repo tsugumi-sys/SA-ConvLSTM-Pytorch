3646983935a54ff516aa2526a2c4c0d96d376e35
tsugumi-sys <tidemark0105@gmail.com>
2024-01-03 12:30:57 +0900

    Adding tests

---
+++ tests/test_model/test_model.py
@@ -0,0 +1,19 @@
+from typing import Tuple
+
+import pytest
+import torch
+
+from tests.test_model.model import TestModel
+
+
+@pytest.mark.parametrize(
+    "return_sequences, expected_output_size",
+    [(True, (5, 6, 3, 16, 16)), (False, (5, 6, 1, 16, 16))],
+)
+def test_TestModel_successfully_build(
+    return_sequences: bool, expected_output_size: Tuple
+):
+    # return_sequence
+    model = TestModel(return_sequences)
+    output = model(torch.rand((5, 6, 3, 16, 16), dtype=torch.float))
+    assert output.size() == expected_output_size
