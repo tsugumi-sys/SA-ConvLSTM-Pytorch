6b5050e6a3db817eeedc5dd2be382b61cc1b2041
tsugumi-sys <tidemark0105@gmail.com>
2024-01-04 18:25:14 +0900

    Refactoring Trainer

--- pipelines/utils/trainer_utils.py
+++ pipelines/utils/trainer_utils.py
@@ -1,9 +1,3 @@
-import os
-from typing import List
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 import torch
 from torch import nn
 
@@ -11,57 +5,6 @@ from torch import nn
 from convlstm.seq2seq import Seq2Seq
 from self_attention_convlstm.seq2seq import SASeq2Seq
 from self_attention_memory_convlstm.seq2seq import SAMSeq2Seq
-
-
-def save_learning_curve_plot(
-    save_dir_path: str,
-    model_name: str,
-    training_losses: List,
-    validation_losses: List,
-) -> None:
-    """create and save leanring curve plot
-
-    Args:
-        save_dir_path (str): Directory path to save figure
-        training_losses (List): training_losses from train(...)
-        validation_losses (List): validation_losses from train(...)
-        validation_accuracy (Optional[List], optional): validation_accuracy from train(...). Defaults to None.
-
-    Raises:
-        ValueError: training_losses and validation_losses must be the same length
-        ValueError: training_losses and validation_accuracy must be the same length
-
-    Returns:
-        str: Saved figure path.
-    """
-    if len(training_losses) != len(validation_losses):
-        raise ValueError("train_losses and validation_losses must be the same length.")
-
-    _, ax = plt.subplots(figsize=(8, 6))
-    ax.set_xlabel("Epochs")
-    x = [i for i in range(len(training_losses))]
-    sns.lineplot(
-        x=x, y=training_losses, label="Training Loss", ax=ax, color="tab:orange"
-    )
-    sns.lineplot(
-        x=x, y=validation_losses, label="Validation Loss", ax=ax, color="tab:blue"
-    )
-    ax.set_ylabel("Training & Validation Loss")
-
-    ax.legend(loc="upper center")
-    plt.tight_layout()
-    save_path = os.path.join(save_dir_path, f"{model_name}_training_results.png")
-    plt.savefig(save_path)
-    plt.close()
-
-
-def save_metrics(
-    save_dir_path: str, model_name: str, training_losses: List, validation_losses: List
-) -> None:
-    df = pd.DataFrame()
-    df["TrainLoss"] = training_losses
-    df["ValidationLoss"] = validation_losses
-    df.to_csv(os.path.join(save_dir_path, f"{model_name}-metrics.csv"))
 
 
 def save_seq2seq_model(model: nn.Module, save_path: str) -> None:
--- tests/pipelines/test_trainer.py
+++ tests/pipelines/test_trainer.py
@@ -1,5 +1,4 @@
 import os
-import random
 import tempfile
 from unittest.mock import patch
 
@@ -22,9 +21,10 @@ def test_run(mocked_save_seq2seq_model):
     mocked_save_seq2seq_model.side_effect = mocked_save_model
     with tempfile.TemporaryDirectory() as tempdirpath:
         model = TestModel()
+        epochs = 3
         trainer = Trainer(
             model=model,
-            train_epochs=1,
+            train_epochs=epochs,
             train_dataloader=mock_data_loader(),
             valid_dataloader=mock_data_loader(),
             loss_criterion=nn.MSELoss(),
@@ -39,85 +39,39 @@ def test_run(mocked_save_seq2seq_model):
             save_metrics_path=os.path.join(tempdirpath, "example.csv"),
         )
         trainer.run()
-        print(os.listdir(tempdirpath))
+
         assert os.path.exists(os.path.join(tempdirpath, "checkpoint.pt"))
-
-
-@patch("pipelines.utils.early_stopping.save_seq2seq_model")
-def test_train(mocked_save_seq2seq_model):
-    with tempfile.TemporaryDirectory() as tempdirpath:
-        model = TestModel()
-        trainer = Trainer(
-            model=model,
-            train_epochs=2,
-            train_dataloader=mock_data_loader(),
-            valid_dataloader=mock_data_loader(),
-            loss_criterion=nn.MSELoss(),
-            accuracy_criterion=nn.L1Loss(),
-            optimizer=Adam(model.parameters(), lr=0.0005),
-            early_stopping=EarlyStopping(
-                patience=30,
-                verbose=True,
-                delta=0.0001,
-            ),
-            save_metrics_path=os.path.join(tempdirpath, "example.csv"),
-        )
-        res = trainer.train()
-        assert len(res["train_loss"]) == 2
-        assert len(res["validation_loss"]) == 2
-        assert len(res["accuracy"]) == 2
-
-
-@patch("pipelines.utils.early_stopping.save_seq2seq_model")
-def test_validation(mocked_save_seq2seq_model):
-    with tempfile.TemporaryDirectory() as tempdirpath:
-        model = TestModel()
-        trainer = Trainer(
-            model=model,
-            train_epochs=2,
-            train_dataloader=mock_data_loader(),
-            valid_dataloader=mock_data_loader(),
-            loss_criterion=nn.MSELoss(),
-            accuracy_criterion=nn.L1Loss(),
-            optimizer=Adam(model.parameters(), lr=0.0005),
-            early_stopping=EarlyStopping(
-                patience=30,
-                verbose=True,
-                delta=0.0001,
-            ),
-            save_metrics_path=os.path.join(tempdirpath, "example.csv"),
-        )
-        valid_loss, acc_loss = trainer.validation()
-        assert isinstance(valid_loss, float)
-        assert isinstance(acc_loss, float)
-
-
-@patch("pipelines.utils.early_stopping.save_seq2seq_model")
-def test_save_metrics_to_csv(mocked_save_seq2seq_model):
-    with tempfile.TemporaryDirectory() as tempdirpath:
-        model = TestModel()
-        save_metrics_path = os.path.join(tempdirpath, "example.csv")
-        trainer = Trainer(
-            model=model,
-            train_epochs=2,
-            train_dataloader=mock_data_loader(),
-            valid_dataloader=mock_data_loader(),
-            loss_criterion=nn.MSELoss(),
-            accuracy_criterion=nn.L1Loss(),
-            optimizer=Adam(model.parameters(), lr=0.0005),
-            early_stopping=EarlyStopping(
-                patience=30,
-                verbose=True,
-                delta=0.0001,
-            ),
-            save_metrics_path=save_metrics_path,
-        )
-        trainer.save_metrics_to_csv(
-            {
-                "train_loss": [random.random()],
-                "validation_loss": [random.random()],
-                "accuracy": [random.random()],
-            },
-            save_metrics_path,
-        )
         assert os.path.exists(os.path.join(tempdirpath, "example.csv"))
+        for metrics in trainer.training_metrics.values():
+            assert len(metrics) == epochs
+
+
+@patch("pipelines.utils.early_stopping.save_seq2seq_model")
+def test_run_early_stopping(mocked_save_seq2seq_model):
+    mocked_save_seq2seq_model.side_effect = mocked_save_model
+    with tempfile.TemporaryDirectory() as tempdirpath:
+        model = TestModel()
+        epochs = 3
+        patience = 1
+        trainer = Trainer(
+            model=model,
+            train_epochs=epochs,
+            train_dataloader=mock_data_loader(),
+            valid_dataloader=mock_data_loader(),
+            loss_criterion=nn.MSELoss(),
+            accuracy_criterion=nn.L1Loss(),
+            optimizer=Adam(model.parameters(), lr=0.0005),
+            early_stopping=EarlyStopping(
+                patience=patience,
+                verbose=True,
+                delta=2,
+                model_save_path=os.path.join(tempdirpath, "checkpoint.pt"),
+            ),
+            save_metrics_path=os.path.join(tempdirpath, "example.csv"),
+        )
+        trainer.run()
+
+        assert os.path.exists(os.path.join(tempdirpath, "checkpoint.pt"))
+        assert os.path.exists(os.path.join(tempdirpath, "example.csv"))
+        for metrics in trainer.training_metrics.values():
+            assert len(metrics) == epochs - patience
