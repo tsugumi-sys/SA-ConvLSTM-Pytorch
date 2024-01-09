4ba0aa60a86d996c84602addc7a194588297376e
tsugumi-sys <tidemark0105@gmail.com>
2024-01-10 00:54:26 +0900

    fix failed tests

--- tests/data_loaders/test_moving_mnist.py
+++ tests/data_loaders/test_moving_mnist.py
@@ -6,15 +6,18 @@ from tests.utils import MockMovingMNIST
 
 @patch("data_loaders.moving_mnist.MovingMNIST")
 def test_MovingMNISTDataLoaders(mocked_MovingMNIST):
-    input_frames = 10
     dataset_length = 10
-    mocked_MovingMNIST.return_value = MockMovingMNIST(
-        dataset_length=dataset_length, split_ratio=input_frames
+    train_batch_size = 2
+    input_frames = 10
+    mocked_MovingMNIST.return_value = MockMovingMNIST(dataset_length=dataset_length)
+    dataloaders = MovingMNISTDataLoaders(
+        train_batch_size=train_batch_size, input_frames=input_frames
     )
-    dataloaders = MovingMNISTDataLoaders(train_batch_size=1, input_frames=input_frames)
-    assert len(dataloaders.train_dataloader) == 7
+    assert len(dataloaders.train_dataloader) == 4
     assert len(dataloaders.validation_dataloader) == 2
     assert len(dataloaders.test_dataloader) == 1
-    # print(next(iter(dataloaders.train_dataloader)))
-    for _, (input, target) in enumerate(dataloaders.train_dataloader, start=1):
-        print(input, target)
+    input, target = next(iter(dataloaders.train_dataloader))
+    assert input.size(0) == train_batch_size
+    assert input.size(2) == input_frames
+    assert target.size(0) == train_batch_size
+    assert target.size(2) == input_frames
