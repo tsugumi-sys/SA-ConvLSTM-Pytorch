721d6308da1c647c5a50a0c6296c15b77cfcd700
tsugumi-sys <tidemark0105@gmail.com>
2024-01-09 21:42:27 +0900

    Use typed params for ConvLSTM

--- tests/convlstm/test_model.py
+++ tests/convlstm/test_model.py
@@ -1,17 +1,18 @@
 import torch
 
-from convlstm.model import ConvLSTM
+from convlstm.model import ConvLSTM, ConvLSTMParams
 from core.constants import DEVICE
 
 
 def test_ConvLSTM():
-    model = ConvLSTM(
-        in_channels=1,
-        out_channels=1,
-        kernel_size=3,
-        padding=1,
-        activation="relu",
-        frame_size=(8, 8),
-    )
+    model_params: ConvLSTMParams = {
+        "in_channels": 1,
+        "out_channels": 1,
+        "kernel_size": 3,
+        "padding": 1,
+        "activation": "relu",
+        "frame_size": (8, 8),
+    }
+    model = ConvLSTM(**model_params)
     output = model(torch.rand((2, 1, 3, 8, 8), dtype=torch.float, device=DEVICE))
     assert output.size() == (2, 1, 3, 8, 8)
