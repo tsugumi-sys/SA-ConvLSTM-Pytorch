8ad3e095a6671b63cf25fdd9930d7b64a29e72df
tsugumi-sys <tidemark0105@gmail.com>
2024-01-09 23:18:31 +0900

    Use typed params for SAMConvLSTM

--- tests/self_attention_memory_convlstm/test_model.py
+++ tests/self_attention_memory_convlstm/test_model.py
@@ -1,18 +1,23 @@
 import torch
 
-from core.constants import DEVICE
-from self_attention_memory_convlstm.model import SAMConvLSTM
+from core.constants import DEVICE, WeightsInitializer
+from self_attention_memory_convlstm.model import SAMConvLSTM, SAMConvLSTMParams
 
 
-def test_ConvLSTM():
-    model = SAMConvLSTM(
-        attention_hidden_dims=1,
-        in_channels=1,
-        out_channels=1,
-        kernel_size=3,
-        padding=1,
-        activation="relu",
-        frame_size=(8, 8),
-    )
+def test_SAMConvLSTM():
+    model_params: SAMConvLSTMParams = {
+        "attention_hidden_dims": 1,
+        "convlstm_params": {
+            "in_channels": 1,
+            "out_channels": 1,
+            "kernel_size": 3,
+            "padding": 1,
+            "activation": "relu",
+            "frame_size": (8, 8),
+            "weights_initializer": WeightsInitializer.He,
+        },
+    }
+
+    model = SAMConvLSTM(**model_params)
     output = model(torch.rand((2, 1, 3, 8, 8), dtype=torch.float, device=DEVICE))
     assert output.size() == (2, 1, 3, 8, 8)
