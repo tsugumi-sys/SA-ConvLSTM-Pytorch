05c0bbc454a7a0c59ddb5949ba62212142869a26
Akira <tidemark0105@gmail.com>
2024-01-15 15:36:46 +0900

    adding split ratios

--- data_loaders/moving_mnist.py
+++ data_loaders/moving_mnist.py
@@ -1,4 +1,5 @@
-from typing import Tuple, Union
+import math
+from typing import List, Tuple, Union
 
 import torch
 from torch.utils.data import DataLoader, Dataset, Subset, random_split
@@ -38,17 +39,25 @@ class MovingMNISTDataLoaders(BaseDataLoaders):
         train_batch_size: int,
         input_frames: int = 10,
         label_frames: int | None = None,
+        split_ratios: List[float] | None = None,
         shuffle: bool = True,
     ):
         self.train_batch_size = train_batch_size
         self.input_frames = input_frames
         self.label_frames = label_frames
         self.shuffle = shuffle
+        if split_ratios is None:
+            split_ratios = [0.7, 0.2, 0.1]
+        if not math.isclose(sum(split_ratios), 1) and sum(split_ratios) > 1:
+            raise ValueError(
+                f"Sum of `split_ratios` (train data, validation data, test data) must be 1.0, but got sum({split_ratios}) = {sum(split_ratios)}."
+            )
+        self.split_ratios = split_ratios
 
         moving_mnist = MovingMNIST(root="./data", download=True)
         train_dataset, valid_dataset, test_dataset = random_split(
             moving_mnist,
-            [0.7, 0.2, 0.1],
+            [*self.split_ratios],
             generator=torch.Generator().manual_seed(42),
         )
         self.train_dataset = VideoPredictionDataset(
