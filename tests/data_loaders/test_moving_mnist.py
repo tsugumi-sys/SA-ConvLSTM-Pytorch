ea0c8ba347839d24018577d673940bfffdb80a56
Akira <tidemark0105@gmail.com>
2024-01-15 15:10:10 +0900

    Adding configurable label fraems

--- tests/data_loaders/test_moving_mnist.py
+++ tests/data_loaders/test_moving_mnist.py
@@ -21,3 +21,25 @@ def test_MovingMNISTDataLoaders(mocked_MovingMNIST):
     assert input.size(2) == input_frames
     assert target.size(0) == train_batch_size
     assert target.size(2) == input_frames
+
+
+@patch("data_loaders.moving_mnist.MovingMNIST")
+def test_MovingMNISTDataLoaders_label_frames_set(mocked_MovingMNIST):
+    dataset_length = 10
+    train_batch_size = 2
+    input_frames = 10
+    label_frames = 1
+    mocked_MovingMNIST.return_value = MockMovingMNIST(dataset_length=dataset_length)
+    dataloaders = MovingMNISTDataLoaders(
+        train_batch_size=train_batch_size,
+        input_frames=input_frames,
+        label_frames=label_frames,
+    )
+    assert len(dataloaders.train_dataloader) == 4
+    assert len(dataloaders.validation_dataloader) == 2
+    assert len(dataloaders.test_dataloader) == 1
+    input, target = next(iter(dataloaders.train_dataloader))
+    assert input.size(0) == train_batch_size
+    assert input.size(2) == input_frames
+    assert target.size(0) == train_batch_size
+    assert target.size(2) == label_frames
